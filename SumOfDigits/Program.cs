51ba85d7be0ece92c8a81649cd47b09c9244dc2b
hallamjason4 <70266362+hallamjason4@users.noreply.github.com>
2021-02-18 16:32:46 +0000

    Add files via upload
    Takes an input number and outputs a sum of all digits

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number to sum: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
